9d7bc0fc8cc7282ffe2c98968b9380665d4e5969
xiongjieyi <jinjingcs@gmail.com>
2020-12-15 21:30:12 +0100

    Updates

---
+++ precache-manifest.2fa7f388f34e03e5582cbfbb827a0e77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4d64a285db63d4cb0995a7e6ea4dd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac231318ac5a3a79c799",
+    "url": "/static/js/2.8b8a2194.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8b8a2194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5e468827fad8cc2174",
+    "url": "/static/js/main.1fd919a0.chunk.js"
+  },
+  {
+    "revision": "27cb89a18ed82c6a7d4d",
+    "url": "/static/js/runtime-main.83a486d1.js"
+  },
+  {
+    "revision": "9720dd8105e2b7d6bd582d601b09f70e",
+    "url": "/static/media/solen.9720dd81.jpg"
+  }
+]);
